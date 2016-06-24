08467f9537083845bbdac354a089614f92810d42
Erik H <erik.holum@cfa.harvard.edu>
2016-06-24 11:31:10 -0400

    Add stub for FunctionModel

---
+++ iris-visualizer/src/main/java/cfa/vo/iris/visualizer/preferences/FunctionModel.java
@@ -0,0 +1,16 @@
+package cfa.vo.iris.visualizer.preferences;
+
+public class FunctionModel {
+    
+    public FunctionModel(SedModel model) {
+    
+    }
+    
+    public LayerModel getFunctionLayerModel() {
+        return null;
+    }
+    
+    public LayerModel getResidualsLayerModel(String residualsOrRatios) {
+        return null;
+    }
+}
